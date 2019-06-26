aa45d8f4c71ee3cd17c74f5f15be47fb8b3d0f44
Boniface Mwenda <boniface.mwenda@andela.com>
2019-06-27 00:30:09 +0300

    List user repos

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,2 +1,20 @@
 class RepositoriesController < ApplicationController
+  include ApiWrapper
+
+  before_action :authenticate_request
+
+  def index
+    response = get("https://api.github.com/users/#{username}/repos")
+    if response.code == '200'
+      render json: response.body, status: :ok
+    else
+      render json: { error: response.message }, status: response.code
+    end
+  end
+
+  private
+
+  def username
+    @username ||= @current_user.username
+  end
 end
